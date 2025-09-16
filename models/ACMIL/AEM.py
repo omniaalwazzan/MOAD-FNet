4c47833004435e4ed223fe1826d4babf4dcc301b
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2025-09-16 09:55:28 -0400

    Add files via upload

---
+++ models/ACMIL/AEM.py
@@ -0,0 +1,538 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 20:48:40 2024
+
+@author: Omnia
+"""
+
+import math
+import os
+import torch
+
+import torch.nn as nn
+from torch import nn, Tensor
+import torch.nn.functional as F
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from collections import defaultdict, deque
+import datetime
+
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision as tv
+from functools import reduce
+from operator import mul
+import torch.distributed as dist
+
+import time
+#%%
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+class SmoothedValue(object):
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        Warning: does not synchronize the deque!
+        """
+        if not is_dist_avail_and_initialized():
+            return
+        t = torch.tensor([self.count, self.total], dtype=torch.float64, device='cuda')
+        dist.barrier()
+        dist.all_reduce(t)
+        t = t.tolist()
+        self.count = int(t[0])
+        self.total = t[1]
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+#%%
+class MetricLogger(object):
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if v is None:
+                continue
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+            type(self).__name__, attr))
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {}".format(name, str(meter))
+            )
+        return self.delimiter.join(loss_str)
+
+    def synchronize_between_processes(self):
+        for meter in self.meters.values():
+            meter.synchronize_between_processes()
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ''
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt='{avg:.4f}')
+        data_time = SmoothedValue(fmt='{avg:.4f}')
+        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
+        log_msg = [
+            header,
+            '[{0' + space_fmt + '}/{1}]',
+            'eta: {eta}',
+            '{meters}',
+            'time: {time}',
+            'data: {data}'
+        ]
+        if torch.cuda.is_available():
+            log_msg.append('max mem: {memory:.0f}')
+        log_msg = self.delimiter.join(log_msg)
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            # if i % print_freq == 0 or i == len(iterable) - 1:
+            #     eta_seconds = iter_time.global_avg * (len(iterable) - i)
+            #     eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+            #     if torch.cuda.is_available():
+            #         print(log_msg.format(
+            #             i, len(iterable), eta=eta_string,
+            #             meters=str(self),
+            #             time=str(iter_time), data=str(data_time),
+            #             memory=torch.cuda.max_memory_allocated() / MB))
+            #     else:
+            #         print(log_msg.format(
+            #             i, len(iterable), eta=eta_string,
+            #             meters=str(self),
+            #             time=str(iter_time), data=str(data_time)))
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('{} Total time: {} ({:.4f} s / it)'.format(
+            header, total_time_str, total_time / len(iterable)))
+
+#%%
+class FCLayer(nn.Module):
+    def __init__(self, in_size, out_size=1):
+        super(FCLayer, self).__init__()
+        self.fc = nn.Sequential(nn.Linear(in_size, out_size))
+
+    def forward(self, feats):
+        x = self.fc(feats)
+        return feats, x
+
+
+class IClassifier(nn.Module):
+    def __init__(self, feature_extractor, feature_size, output_class):
+        super(IClassifier, self).__init__()
+
+        self.feature_extractor = feature_extractor
+        self.fc = nn.Linear(feature_size, output_class)
+
+    def forward(self, x):
+        device = x.device
+        feats = self.feature_extractor(x)  # N x K
+        c = self.fc(feats.view(feats.shape[0], -1))  # N x C
+        return feats.view(feats.shape[0], -1), c
+
+
+class BClassifier(nn.Module):
+    def __init__(self, conf, dropout_v=0.0, nonlinear=True, passing_v=False,
+                 confounder_path=False):  # K, L, N
+        super(BClassifier, self).__init__()
+        input_size=conf.D_feat
+        output_class=conf.n_class
+        if nonlinear:
+            self.q = nn.Sequential(nn.Linear(input_size, conf.D_inner), nn.ReLU(), nn.Linear(conf.D_inner, 128), nn.Tanh())
+        else:
+            self.q = nn.Linear(input_size, conf.D_inner)
+        if passing_v:
+            self.v = nn.Sequential(
+                nn.Dropout(dropout_v),
+                nn.Linear(input_size, input_size),
+                nn.ReLU()
+            )
+        else:
+            self.v = nn.Identity()
+
+        ### 1D convolutional layer that can handle multiple class (including binary)
+        self.fcc = nn.Conv1d(output_class, output_class, kernel_size=input_size)
+
+
+    def forward(self, feats, c):  # N x K, N x C
+        device = feats.device
+        V = self.v(feats)  # N x V, unsorted
+        Q = self.q(feats).view(feats.shape[0], -1)  # N x Q, unsorted
+        # handle multiple classes without for loop
+        _, m_indices = torch.sort(c, 0,
+                                  descending=True)  # sort class scores along the instance dimension, m_indices in shape N x C
+        # print(m_indices.shape)
+        m_feats = torch.index_select(feats, dim=0,
+                                     index=m_indices[0, :])  # select critical instances, m_feats in shape C x K
+        q_max = self.q(m_feats)  # compute queries of critical instances, q_max in shape C x Q
+        A = torch.mm(Q, q_max.transpose(0,
+                                        1))  # compute inner product of Q to each entry of q_max, A in shape N x C, each column contains unnormalized attention scores
+        A = A / torch.sqrt(torch.tensor(Q.shape[1], dtype=torch.float32, device=device))  # normalize attention scores, A in shape N x C,
+        A = A.transpose(0, 1)
+
+        A_out = A
+        A = F.softmax(A, dim=-1)
+        B = torch.mm(A, V)  # compute bag representation, B in shape C x V
+        B = B.view(1, B.shape[0], B.shape[1])  # 1 x C x V
+
+        C = self.fcc(B)  # 1 x C x 1
+        C = C.view(1, -1)
+        return C, A_out, B
+
+
+class MILNet(nn.Module):
+    def __init__(self, i_classifier, b_classifier):
+        super(MILNet, self).__init__()
+        self.i_classifier = i_classifier
+        self.b_classifier = b_classifier
+
+    def forward(self, x):
+        feats, classes = self.i_classifier(x[0])
+        # print(feats)
+        prediction_bag, A, B = self.b_classifier(feats, classes)
+        return classes, prediction_bag, A
+#%%
+
+class Classifier_1fc(nn.Module):
+    def __init__(self, n_channels, n_classes, droprate=0.0):
+        super(Classifier_1fc, self).__init__()
+        self.fc = nn.Linear(n_channels, n_classes)
+        self.droprate = droprate
+        if self.droprate != 0.0:
+            self.dropout = torch.nn.Dropout(p=self.droprate)
+
+    def forward(self, x):
+
+        if self.droprate != 0.0:
+            x = self.dropout(x)
+        x = self.fc(x)
+        return x
+
+
+class residual_block(nn.Module):
+    def __init__(self, nChn=512):
+        super(residual_block, self).__init__()
+        self.block = nn.Sequential(
+                nn.Linear(nChn, nChn, bias=False),
+                nn.ReLU(inplace=True),
+                nn.Linear(nChn, nChn, bias=False),
+                nn.ReLU(inplace=True),
+            )
+    def forward(self, x):
+        tt = self.block(x)
+        x = x + tt
+        return x
+
+
+class DimReduction(nn.Module):
+    def __init__(self, n_channels, m_dim=512, numLayer_Res=0):
+        super(DimReduction, self).__init__()
+        self.fc1 = nn.Linear(n_channels, m_dim, bias=False)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.numRes = numLayer_Res
+
+        self.resBlocks = []
+        for ii in range(numLayer_Res):
+            self.resBlocks.append(residual_block(m_dim))
+        self.resBlocks = nn.Sequential(*self.resBlocks)
+
+    def forward(self, x):
+
+        x = self.fc1(x)
+        x = self.relu1(x)
+
+        if self.numRes > 0:
+            x = self.resBlocks(x)
+
+        return x
+
+
+
+
+
+
+#%%
+
+
+
+class MHA(nn.Module):
+    def __init__(self, conf):
+        super(MHA, self).__init__()
+        self.dimreduction = DimReduction(conf.D_feat, conf.D_inner)
+        self.attention = MultiHeadAttention(conf.D_inner, 8)
+        self.q = nn.Parameter(torch.zeros((1, 1, conf.D_inner)))
+        nn.init.normal_(self.q, std=1e-6)
+        self.n_class = conf.n_class
+        self.classifier = Classifier_1fc(conf.D_inner, conf.n_class, 0.0)
+
+    def forward(self, input, is_train=False):
+        input = self.dimreduction(input)
+        q = self.q
+        k = input
+        v = input
+        feat, attn = self.attention(q, k, v)
+        output = self.classifier(feat)
+
+        return output, attn
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    An attention layer that allows for downscaling the size of the embedding
+    after projection to queries, keys, and values.
+    """
+
+    def __init__(
+            self,
+            embedding_dim: int,
+            num_heads: int,
+            downsample_rate: int = 1,
+            dropout: float = 0.1,
+    ) -> None:
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.internal_dim = embedding_dim // downsample_rate
+        self.num_heads = num_heads
+        assert self.internal_dim % num_heads == 0, "num_heads must divide embedding_dim."
+
+        self.q_proj = nn.Linear(embedding_dim, self.internal_dim)
+        self.k_proj = nn.Linear(embedding_dim, self.internal_dim)
+        self.v_proj = nn.Linear(embedding_dim, self.internal_dim)
+        self.out_proj = nn.Linear(self.internal_dim, embedding_dim)
+
+        self.layer_norm = nn.LayerNorm(embedding_dim, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def _separate_heads(self, x: Tensor, num_heads: int) -> Tensor:
+        b, n, c = x.shape
+        x = x.reshape(b, n, num_heads, c // num_heads)
+        return x.transpose(1, 2)  # B x N_heads x N_tokens x C_per_head
+
+    def _recombine_heads(self, x: Tensor) -> Tensor:
+        b, n_heads, n_tokens, c_per_head = x.shape
+        x = x.transpose(1, 2)
+        return x.reshape(b, n_tokens, n_heads * c_per_head)  # B x N_tokens x C
+
+    def forward(self, q: Tensor, k: Tensor, v: Tensor) -> Tensor:
+        # Input projections
+        q = self.q_proj(q)
+        k = self.k_proj(k)
+        v = self.v_proj(v)
+
+        # Separate into heads
+        q = self._separate_heads(q, self.num_heads)
+        k = self._separate_heads(k, self.num_heads)
+        v = self._separate_heads(v, self.num_heads)
+
+        # Attention
+        _, _, _, c_per_head = q.shape
+        attn = q @ k.permute(0, 1, 3, 2)  # B x N_heads x N_tokens x N_tokens
+        attn = attn / math.sqrt(c_per_head)
+
+        attn_out = attn
+        attn = torch.softmax(attn, dim=-1)
+        # Get output
+        out1 = attn @ v
+        out1 = self._recombine_heads(out1)
+        out1 = self.out_proj(out1)
+        out1 = self.dropout(out1)
+        out1 = self.layer_norm(out1)
+
+        #return out1[0], attn_out[0]
+        return out1[0], attn_out[0]
+
+class Attention_Gated(nn.Module):
+    def __init__(self, L=512, D=128, K=1):
+        super(Attention_Gated, self).__init__()
+
+        self.L = L
+        self.D = D
+        self.K = K
+
+        self.attention_V = nn.Sequential(
+            nn.Linear(self.L, self.D),
+            nn.Tanh()
+        )
+
+        self.attention_U = nn.Sequential(
+            nn.Linear(self.L, self.D),
+            nn.Sigmoid()
+        )
+
+        self.attention_weights = nn.Linear(self.D, self.K)
+
+    def forward(self, x):
+        ## x: N x L
+        A_V = self.attention_V(x)  # NxD
+        A_U = self.attention_U(x)  # NxD
+        A = self.attention_weights(A_V * A_U)  # NxK
+        A = torch.transpose(A, 1, 0)  # KxN
+
+        return A  ### K x N
+
+
+class ABMIL(nn.Module):
+    def __init__(self, conf, D=128, droprate=0):
+        super(ABMIL, self).__init__()
+        self.dimreduction = DimReduction(conf.D_feat, conf.D_inner)
+        self.attention = Attention_Gated(conf.D_inner, D, 1)
+        self.classifier = Classifier_1fc(conf.D_inner, conf.n_class, droprate)
+
+    def forward(self, x, is_train=False):  ## x: N x L
+        x = x[0]
+        med_feat = self.dimreduction(x)
+        A = self.attention(med_feat)  ## K x N
+
+        A_out = A
+        A = F.softmax(A, dim=1)  # softmax over N
+        afeat = torch.mm(A, med_feat)  ## K x L
+        outputs = self.classifier(afeat)
+        return outputs, A_out.unsqueeze(0)
+
+
+#%%
+
+
+class ACMIL(nn.Module):
+    def __init__(self, conf, D=128, droprate=0, n_masked_patch=10, n_token=1, mask_drop=0.6):
+        super(ACMIL, self).__init__()
+        self.dimreduction = DimReduction(conf.D_feat, conf.D_inner)
+        self.attention = Attention_Gated(conf.D_inner, D, n_token)
+        self.classifier = nn.ModuleList()
+        for i in range(n_token):
+            self.classifier.append(Classifier_1fc(conf.D_inner, conf.n_class, droprate))
+        self.n_masked_patch = n_masked_patch
+        self.n_token = n_token
+        self.Slide_classifier = Classifier_1fc(conf.D_inner, conf.n_class, droprate)
+        self.mask_drop = mask_drop
+
+    def forward(self, x):  ## x: N x L
+        x = x[0]  # 1 * 1000 * 512
+        x = self.dimreduction(x)  # 1000 * 256
+        A = self.attention(x)  ##  1000 * 1
+
+        if self.n_masked_patch > 0 and self.training and self.mask_drop > 0:
+            # Get the indices of the top-k largest values
+            k, n = A.shape
+            n_masked_patch = min(self.n_masked_patch, n)
+            # n_masked_patch = int(n * 0.01)
+            _, indices = torch.topk(A, n_masked_patch, dim=-1)
+            rand_selected = torch.argsort(torch.rand(*indices.shape), dim=-1)[:, :int(n_masked_patch * self.mask_drop)]
+            masked_indices = indices[torch.arange(indices.shape[0]).unsqueeze(-1), rand_selected]
+            random_mask = torch.ones(k, n).to(A.device)
+            random_mask.scatter_(-1, masked_indices, 0)
+            A = A.masked_fill(random_mask == 0, -1e9)
+
+        A_out = A
+        A = F.softmax(A, dim=1)  # softmax over N
+        afeat = torch.mm(A, x)  ## K x L
+        outputs = []
+        for i, head in enumerate(self.classifier):
+            outputs.append(head(afeat[i]))
+        bag_A = F.softmax(A_out, dim=1).mean(0, keepdim=True)
+        bag_feat = torch.mm(bag_A, x)
+        return torch.stack(outputs, dim=0), self.Slide_classifier(bag_feat), A_out.unsqueeze(0)
+
+    def forward_feature(self, x, use_attention_mask=False):  ## x: N x L
+        x = x[0]
+        x = self.dimreduction(x)
+        A = self.attention(x)  ## K x N
+
+        if self.n_masked_patch > 0 and use_attention_mask:
+            # Get the indices of the top-k largest values
+            k, n = A.shape
+            n_masked_patch = min(self.n_masked_patch, n)
+            _, indices = torch.topk(A, n_masked_patch, dim=-1)
+            rand_selected = torch.argsort(torch.rand(*indices.shape), dim=-1)[:, :int(n_masked_patch * self.mask_drop)]
+            masked_indices = indices[torch.arange(indices.shape[0]).unsqueeze(-1), rand_selected]
+            random_mask = torch.ones(k, n).to(A.device)
+            random_mask.scatter_(-1, masked_indices, 0)
+            A = A.masked_fill(random_mask == 0, -1e9)
+
+        A_out = A
+        bag_A = F.softmax(A_out, dim=1).mean(0, keepdim=True)
+        bag_feat = torch.mm(bag_A, x)
+        return bag_feat
+
+
+#%%
+# Create a random input tensor with shape (batch_size, num_tokens, D_feat)
+# input_tensor = torch.randn(1, 1024, 128)  # For example, batch_size=1, num_tokens=50
+
+# # Forward pass through the model
+# output, attention = model(input_tensor)
+
+# # Print the output and attention
+# print("Output:", output)
+# print("Attention:", attention)
